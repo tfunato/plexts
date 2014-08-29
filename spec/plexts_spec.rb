26eaa17b2ddf72827f067ac274327de0e7c84573
tfunato <tfunato@healthcare.ne.jp>
2014-08-29 12:36:50 +0900

    modify spec

--- spec/plexts_spec.rb
+++ spec/plexts_spec.rb
@@ -1,8 +1,9 @@
 require 'spec_helper'
 
 describe Plexts do
+  # TODO add test
   #Plexts.get_entities(35.577807, 139.629278)
-  Plexts.to_console
+  #Plexts.to_console
   #Plexts.get_plexts
   #Plexts.get_artifacts()
 end
